53165e85eae1bb1c56505291996e7e39d0dcd5c2
Facundo <diazortizfacundo@gmail.com>
2026-06-15 16:14:54 -0300

    tercer commit

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -5,13 +5,15 @@ Console.WriteLine("Hello, World!");
 
 //Console.Write("Ingrese el path del directorio que desea analizar: ");
 //string path = Console.ReadLine();
-string path = @"C:\Users\Alumno\Desktop\tp9\tl1-tp9-2026-Facundo812";
+string path = @"C:\tp9\tl1-tp9-2026-Facundo812";
 
 //controlar que exista el path
 /*while(!Directory.Exists(path)){
     Console.Write("El directorio no existe, ingrese otro path: ");
     path = Console.ReadLine();
 }*/
+
+string reporte = path + @"\reporte_archivos.csv";
 
 String[] directorios = Directory.GetDirectories(path);
 String[] archivos = Directory.GetFiles(path);
@@ -20,10 +22,20 @@ foreach(string s in directorios){
     Console.WriteLine("Carpeta: "+ Path.GetFileName(s));
 }
 
-FileInfo info = new FileInfo();
+string[] matriz = new string[archivos.Length];
+int i = 0;
+
 foreach(string s in archivos){
-    Console.WriteLine("Archivo: "+ Path.GetFileName(s));
+    FileInfo F = new FileInfo(s);
+    double tamKB = (float)F.Length/1024;
+
+    Console.WriteLine("Archivo: "+Path.GetFileName(s)+", tamanio: "+tamKB.ToString("F2")+"KB");
+
+    matriz[i] = Path.GetFileName(s)+"   "+tamKB.ToString("F2")+"KB   "+F.LastWriteTime;
+    i++;
 }
+
+File.WriteAllLines(reporte,matriz);
 
 
 
